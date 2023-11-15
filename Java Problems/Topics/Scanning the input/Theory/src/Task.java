c36ecc4f5f2c726995019249a4edfd73d356ade5
iicakeii <marccharles243@gmail.com>
2023-11-15 05:36:07 -0500

    first commit

---
+++ Java Problems/Topics/Scanning the input/Theory/src/Task.java
@@ -0,0 +1,13 @@
+// You can experiment here, it won’t be checked
+
+import java.util.Scanner;
+
+public class Task {
+  public static void main(String[] args) {
+    // put your code here
+    Scanner scanner = new Scanner(System.in);
+    // start coding here
+    String line = scanner.next();
+    System.out.println(line);
+  }
+}
