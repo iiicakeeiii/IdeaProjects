58ec177b2268fb0c82cf6738f0a2d452eb658d78
iicakeii <marccharles243@gmail.com>
2024-02-01 15:49:32 -0500

    update

--- CTP250/ckLab1/Restaurant.cpp
+++ CTP250/ckLab1/Restaurant.cpp
@@ -10,19 +10,31 @@ void displayData();
 
 int main() {
 	vector<string> restaurantData;
+
+	
 	readData("restaurantLab1.txt");
-}
+}//end main();
+
 void readData(string fileName) {
 	ifstream infile(fileName);
 	string line = "";
+	vector<string> restaurantData;
 
 	if (!infile.is_open()) {
 		cout << "file not found... exiting...";
 		exit(0);
-	}//end if
+	}//end if;
 
-	while (!infile.eof) {
+	while (!infile.eof()) {
+		
 		getline(infile, line);
-		istringstream iss(line);
-	}
-}
+		restaurantData.push_back(line);
+	}//end while;
+
+	int size = restaurantData.size();
+	for (int i = 0; i < size; i++) {
+		cout << restaurantData[i];
+	}//end for;
+	
+	infile.close();
+}//end readData();
