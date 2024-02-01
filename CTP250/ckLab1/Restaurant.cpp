cac105ff0049efe2d19e5c5a952b2d1657edb198
iicakeii <marccharles243@gmail.com>
2024-02-01 16:13:39 -0500

    generic update

--- CTP250/ckLab1/Restaurant.cpp
+++ CTP250/ckLab1/Restaurant.cpp
@@ -5,20 +5,18 @@
 
 using namespace std;
 
-void readData(string fileName);
+void readData(const vector<string>&, const string&);
 void displayData();
 
 int main() {
 	vector<string> restaurantData;
-
-	
-	readData("restaurantLab1.txt");
+	string fileName = "restaurantLab1.txt";
+	readData(restaurantData, fileName);
 }//end main();
 
-void readData(string fileName) {
+void readData(const vector<string>& restaurantData, const string& fileName) {
 	ifstream infile(fileName);
 	string line = "";
-	vector<string> restaurantData;
 
 	if (!infile.is_open()) {
 		cout << "file not found... exiting...";
@@ -30,10 +28,11 @@ void readData(string fileName) {
 		getline(infile, line);
 		restaurantData.push_back(line);
 	}//end while;
+	cin.ignore();
 
 	int size = restaurantData.size();
 	for (int i = 0; i < size; i++) {
-		cout << restaurantData[i];
+		cout << "data: " << restaurantData[i] << endl;
 	}//end for;
 	
 	infile.close();
